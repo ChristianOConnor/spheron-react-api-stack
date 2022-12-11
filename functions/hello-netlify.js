829e996ad6e2273dbd57f75f7f66e62d0e5a94a0
ChristianOConnor <warpanomaly@gmail.com>
2022-12-11 04:57:38 -0500

    google function works with credentials file

--- functions/hello-netlify.js
+++ functions/hello-netlify.js
@@ -1,6 +1,17 @@
+const GoogleAuth = require("google-auth-library").GoogleAuth;
+
 exports.handler = async (event) => {
+  const auth = new GoogleAuth({
+    keyFilename: "<CREDENTIAL PATH>"
+  });
+  const url = process.env.RUN_APP_URL;
+
+  //Create your client with an Identity token.
+	const client = await auth.getIdTokenClient(url)
+	const result = await client.request({ url })
+
   return {
     statusCode: 200,
-    body: process.env.GREETING_TEST,
+    body: result.data,
   };
 };
