ecd89aef19459c5a0030ae7b8478bb13894bcb55
ChristianOConnor <warpanomaly@gmail.com>
2023-01-17 19:41:49 -0500

    give toggle switch heading prettier

--- frontend/src/ToggleSwitch.tsx
+++ frontend/src/ToggleSwitch.tsx
@@ -4,10 +4,15 @@ import "./ToggleSwitch.css";
 function ToggleSwitch({ toggledVal, setToggled } : { toggledVal: boolean, setToggled: Dispatch<SetStateAction<boolean>> }) {
   const onToggle = () => setToggled(!toggledVal);
   return (
-    <label className="toggle-switch">
-      <input type="checkbox" checked={toggledVal} onChange={onToggle} />
-      <span className="switch" />
-    </label>
+    <div className="card">
+      <h2>
+        {"Testing On/Off Switch"}
+      </h2>
+      <label className="toggle-switch">
+        <input type="checkbox" checked={toggledVal} onChange={onToggle} />
+        <span className="switch" />
+      </label>
+    </div>
   );
 }
 export default ToggleSwitch;
